ff3378eb20c24b35aba82a980fb2a7eaaa93dca7
Léu Almeida <leo@webid.net.br>
2019-08-22 21:15:49 -0300

    Novas Rotas

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import Dashboard from '../dashboard/dashboard'
-import BillingCycle from '../billingCycle/billingCycle'
 import Clients from '../clients/clients'
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={Dashboard} />
-        <Route path ='/billingCycles' component={BillingCycle} />
         <Route path ='/clients' component={Clients} />
         <Redirect from='*' to='/' />
     </Router>
