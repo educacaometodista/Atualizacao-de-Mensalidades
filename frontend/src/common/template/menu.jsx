930abba67007b5144846fdf506ad4c36160c412f
Léu Almeida <leo@webid.net.br>
2019-08-22 21:15:43 -0300

    Alteração no Menu

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -5,13 +5,8 @@ import MenuTree from './menuTree'
 export default props => (
     <ul className='sidebar-menu'>
         <MenuItem path='#' label='Dashboard' icon='dashboard' />
-        <MenuTree label='Financeiro' icon='usd'>
-            <MenuItem path='#billingCycles'
-                label='Ciclo de Pagamentos' icon='edit' />
-        </MenuTree>
-        <MenuTree label='Clientes' icon='users'>
-            <MenuItem path='#clients'
-                label='Cadastro de Clientes' icon='edit' />
+        <MenuTree label='Mensalidades' icon='users'>
+            <MenuItem path='#clients' label='UMESP' icon='edit' />
         </MenuTree>
     </ul>
 )
