a20f97254ca5ff49628cae0e93142bbf3fa8c425
Léu Almeida <leo@webid.net.br>
2019-09-28 22:05:16 -0300

    Página de Mensalidades - UNIMEP

--- msWallet/src/views/app/mensalidades/unimep.js
+++ msWallet/src/views/app/mensalidades/unimep.js
@@ -1,12 +1,9 @@
 import React, { Component, Fragment } from "react";
 import { Row } from "reactstrap";
 
-import CadastrarAtivo from "../../../components/common/cadastrarAtivo";
 import { Colxx, Separator } from "../../../components/common/CustomBootstrap";
 import Breadcrumb from "../../../containers/navs/Breadcrumb";
 import '../../../assets/css/common/style.css';
-import StocksTitle from '../../../components/common/stocksTitle'
-import Icon from '../../../components/common/icon'
 
 import Tabletop from 'tabletop';
 
@@ -20,10 +17,10 @@ export default class Unimep extends Component {
 
   componentDidMount() {
     Tabletop.init({
-      key: '1L-ywhumD0TnbJVxhMK_zZ9hnPsSKA86xPen1XCqNuG8',
-      orderby: 'Posição',
+      key: '1QpPgpAKyZXvwirnHekmSWqYsaT7s0B4Szd0UsYEEPJw',
+      orderby: 'Curso',
       reverse: false,
-      wanted: ['FIIs'],
+      wanted: ['UNIMEP'],
 
       callback: googleData => {
         this.setState({
@@ -37,75 +34,62 @@ export default class Unimep extends Component {
 
     render() {
       const { data } = this.state
+      const ie = "http://unimep.edu.br/"
+      // const weg = data[0]
+      // console.log(weg)
+
         return (
             <Fragment>
             <Row>
-              <Colxx xxs="10">
-                <Breadcrumb heading="menu.fiis" match={this.props.match} />
-              </Colxx>
-              <Colxx xxs="2">                
-              {data.map(obj => {
-                  return (
-                    <span key={obj.Total} className={obj.Arrow === 'arrow-up-2' ? 'text-success' : 'text-danger'}>
-                      <h1>
-                        {obj.Total}
-                        <Icon icon={obj.Arrow} />
-                      </h1>
-                    </span>
-                  )
-                })}
+              <Colxx xxs="6">
+                <Breadcrumb heading="Universidade Metodista de Piracicaba" match={this.props.match} />
               </Colxx>
               <Colxx xxs="12">
-              <Separator className="mb-5" />
+                <Separator className="mb-5" />
               </Colxx>
             </Row>
 
             <Row>
-              <Colxx xxs="12">
-                <StocksTitle />
-              </Colxx>
-            </Row>
-
-            <Row>
-              {
-                  data.map(obj => {
-                    return (
-                      <Colxx xs="12" className="mb-4" key={obj.id}>
-
-                        <div className="d-flex flex-row active card">
-                          <img alt={obj.Nome} src={obj.Logo} className="list-thumbnail responsive border-0 card-img-left" />
-                        <div className={'pl-2 d-flex flex-grow-1 min-width-zero'}>
-                          <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
-                              <p className="list-item-heading mb-1 truncate">{obj.Nome}</p>
-                            <p className="mb-1 text-small w-15 w-sm-100">{obj.Ticker}</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Preço}</p>
-                            <p className="mb-1 text-small w-15 w-sm-100">{obj.Posição}</p>
-                            <p className="mb-1  text-small w-15 w-sm-100">{obj.Lucro}</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.PM}</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Rentabilidade}</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Falta}</p>
-                            <h4 className="w-15 w-sm-100">
-                              <a href={obj.FaltaCotas >= 0 ? 'http://xpinvestimentos.com.br' : '#'} target={obj.FaltaCotas >= 0 ? '_blank' : '_self'}>
-                                <span className={`badge badge-${obj.Situação === 'Comprar' ? 'success' : 'outline-danger'} badge-pill`}>{obj.Situação}
-                                </span>
+              <Colxx xs="12">
+              <div className="table-responsive">
+                      <table className="table table-hover">
+                        <thead className="bg-dark">
+                          <tr>
+                            <th>Nome do Curso</th>
+                            <th>Referência</th>
+                            <th>Valor Real</th>
+                            <th>Valor com Desconto</th>
+                            <th>Página do Curso</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        {
+                          data.map(obj => {
+                            return (
+                              <tr key={obj._id}>    
+                                <td id={`curso-${obj.Curso}`.replace(/\s/g, '-').toLowerCase()}>{obj.Curso}</td>
+                                <td id={`referencia-${obj.Curso}`.replace(/\s/g, '-').toLowerCase()}>{obj.Referência}</td>
+                                <td id={`valor-${obj.Curso}`.replace(/\s/g, '-').toLowerCase()}>{obj.Valor}</td>
+                                <td id={`antecipado-${obj.Curso}`.replace(/\s/g, '-').toLowerCase()} className="text-success"><strong>{obj.Antecipado}</strong></td>
+                                <td>
+                                <a 
+                                href={ie+obj.Curso.toLowerCase().replace(/\s/g, '-')} 
+                                target="_blank"
+                                className="btn btn-success btn-sm">
+                                  Conhecer Curso
                                 </a>
-                            </h4>
-                          </div>
-                        </div>
-                        </div>
-                      </Colxx>
-                    )
-                  })
-                }
+                                </td>
+                              </tr>
+                            )
+                          })
+                        }
+                        </tbody>
+                      </table>
+                    </div>
+              </Colxx>
               </Row>
-              <Row>
-                <Colxx xs="12" className="mb-4 mt-4 text-center">
-                  <h2>Cadastrar novo ativo <span className="badge badge-info">BETA</span></h2>
-                </Colxx>
-                <Colxx xs="12" className="mb-4">
-                  <CadastrarAtivo />
-                </Colxx>
-              </Row>
+            
+
           </Fragment>
         )
     }
