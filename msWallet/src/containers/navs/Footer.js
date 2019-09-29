533fe8a415691846004e0ff371bdfc669c3540d9
Léu Almeida <leo@webid.net.br>
2019-09-28 22:21:25 -0300

    Correções do Rodapé

--- msWallet/src/containers/navs/Footer.js
+++ msWallet/src/containers/navs/Footer.js
@@ -15,11 +15,17 @@ class Footer extends Component {
     return (
         <nav className="d-flex footer fixed-bottom">
           <div className="d-flex pl-5 text-white align-items-left">
-            Copyright &copy; <a href="https://github.com/educacaometodista" className="">Educação Metodista</a>
+            Copyright &copy; 2019 
+            <a href="https://github.com/educacaometodista" className="">Educação Metodista.</a> 
+            <div className="mr-2 ml-2">
+              <a href="https://github.com/educacaometodista/Atualizacao-de-Mensalidades" className="badge badge-light badge-pill">
+                <i className="fab fa-github text-dark"></i>
+              </a>
+            </div>
           </div>
 
           <div className="logoFooter align-items-right">
-            {/* <img src="http://portal.metodista.br/msg/institucional/comunicados-2018/imagens/educacao-metodista.png" /> */}
+            <img src="http://portal.metodista.br/msg/institucional/comunicados-2018/imagens/educacao-metodista.png" />
           </div>
         </nav>
     );
